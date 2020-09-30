0c7a9aa8887ab2d3187553a6fefddd7717783588
Jacob <jacob.vecera@gmail.com>
2020-09-30 18:17:23 +0200

    dropwdown

---
+++ resources/assets/js/components/dropdown/Dropdown.vue
@@ -0,0 +1,59 @@
+<template>
+    <div class="relative py-4">
+        <button @click="toggleShow" class="focus:outline-none">
+            <slot name="header" :show="show"></slot>
+        </button>
+        <div
+            class="dropdown-content"
+            :class="{
+                'dropdown-closed': !show,
+                'right-0': position == 'right',
+            }"
+        >
+            <slot name="content"></slot>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    props: ["position"],
+
+    data() {
+        return {
+            show: false,
+        };
+    },
+
+    methods: {
+        open() {
+            this.show = true;
+        },
+        close() {
+            this.show = false;
+        },
+        toggleShow() {
+            this.show = !this.show;
+        },
+    },
+};
+</script>
+
+<style scoped>
+.dropdown-content::before {
+    content: "";
+    top: 0;
+    right: 1rem;
+    @apply absolute transform -translate-y-full w-0 h-0;
+    border-style: solid;
+    border-width: 0 15px 9px 15px;
+    border-color: transparent transparent #fff transparent;
+}
+.dropdown-content {
+    @apply absolute bottom-0 shadow-lg p-6 transform translate-y-full w-40 rounded-lg transition duration-300;
+}
+.dropdown-closed {
+    @apply opacity-0 pointer-events-none;
+    transform: translateY(90%);
+}
+</style>
---
+++ resources/assets/js/components/dropdown/DropdownUser.vue
@@ -0,0 +1,34 @@
+<template>
+    <div>
+        <dropdown position="right">
+            <template #header="{ show }">
+                <div class="flex items-center font-bold">
+                    Příklad
+
+                    <svg
+                        class="w-3 ml-3 stroke-current transition duration-300"
+                        :class="{ 'transform -rotate-180': !show }"
+                        viewBox="0 0 12 7"
+                        xmlns="http://www.w3.org/2000/svg"
+                        xmlns:xlink="http://www.w3.org/1999/xlink"
+                    >
+                        <polyline
+                            fill="none"
+                            stroke-width="2"
+                            points="1 1 6 6 11 1"
+                        ></polyline>
+                    </svg>
+                </div>
+                <div class="text-xs pt-1">Administrátor</div>
+            </template>
+
+            <template v-slot:content>
+                <a href="#" class="text-red-500 font-bold">Odhlásit se</a>
+            </template>
+        </dropdown>
+    </div>
+</template>
+
+<script>
+export default {};
+</script>
