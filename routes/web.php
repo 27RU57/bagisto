49d1ed6688b70adb0c500fd4f57ac637110b3a4e
Jacob <jacob.vecera@gmail.com>
2020-09-30 21:14:43 +0200

    add route + names

--- routes/web.php
+++ routes/web.php
@@ -1,22 +1,26 @@
 <?php
 
 Route::prefix('custom')->group(function () {
-    Route::view('/dashboard', 'admin.dashboard');
+    Route::view('/dashboard', 'admin.dashboard')->name('admin.dashboard');
+
+    Route::group(['prefix' => 'orders'], function () {
+        Route::view('/', 'admin.orders.index')->name('admin.orders');
+    });
 
     Route::group(['prefix' => 'products'], function () {
-        Route::view('/', 'admin.products.index');
+        Route::view('/', 'admin.products.index')->name('admin.products');
         Route::view('/create', 'admin.products.create');
         Route::view('/edit', 'admin.products.edit');
     });
 
     Route::group(['prefix' => 'recipes'], function () {
-        Route::view('/', 'admin.recipes.index');
+        Route::view('/', 'admin.recipes.index')->name('admin.recipes');
         Route::view('/create', 'admin.recipes.create');
         Route::view('/edit', 'admin.recipes.edit');
     });
 
     Route::group(['prefix' => 'blog'], function () {
-        Route::view('/', 'admin.blog.index');
+        Route::view('/', 'admin.blog.index')->name('admin.blog');
         Route::view('/create', 'admin.blog.create');
         Route::view('/edit', 'admin.blog.edit');
     });
