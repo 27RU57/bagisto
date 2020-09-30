a1fd46c39f9a6ca10170fcaa6bc4b982c6983a75
Jacob <jacob.vecera@gmail.com>
2020-09-30 09:36:17 +0200

    admin routes (at the moment in web routes)

--- routes/web.php
+++ routes/web.php
@@ -1,12 +1,23 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+Route::prefix('custom')->group(function () {
+    Route::view('/dashboard', 'admin.dashboard');
+
+    Route::group(['prefix' => 'products'], function () {
+        Route::view('/', 'admin.products.index');
+        Route::view('/create', 'admin.products.create');
+        Route::view('/edit', 'admin.products.edit');
+    });
+
+    Route::group(['prefix' => 'recipes'], function () {
+        Route::view('/', 'admin.recipes.index');
+        Route::view('/create', 'admin.recipes.create');
+        Route::view('/edit', 'admin.recipes.edit');
+    });
+
+    Route::group(['prefix' => 'blog'], function () {
+        Route::view('/', 'admin.blog.index');
+        Route::view('/create', 'admin.blog.create');
+        Route::view('/edit', 'admin.blog.edit');
+    });
+});
