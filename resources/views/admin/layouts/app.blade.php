f8d47df018383d36c2bcee5a24770346a77eac65
Jacob <jacob.vecera@gmail.com>
2020-09-30 09:35:58 +0200

    views

---
+++ resources/views/admin/layouts/app.blade.php
@@ -0,0 +1,18 @@
+<!doctype html>
+<html lang="{{ app()->getLocale() }}">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <!-- Styles -->
+    <link href="{{ mix('css/app.css') }}" rel="stylesheet">
+</head>
+
+<body>
+    <div id="app">
+        @yield('content')
+    </div>
+</body>
