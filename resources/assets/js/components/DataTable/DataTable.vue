18d221e5a1e2b2b7ff88b0925c7415c8a2314a99
Jacob <jacob.vecera@gmail.com>
2020-10-01 08:47:49 +0200

    data table

---
+++ resources/assets/js/components/DataTable/DataTable.vue
@@ -0,0 +1,57 @@
+<template>
+    <div>
+        <table class="data-table">
+            <thead>
+                <tr>
+                    <td v-for="(col, i) in cols" :key="`col-${i}`">
+                        {{ col.text }}
+                    </td>
+                    <td v-if="actions">Akce</td>
+                </tr>
+            </thead>
+            <tbody>
+                <tr v-for="(row, r) in rows" :key="`row-${r}`">
+                    <TableRows
+                        v-for="(col, c) in cols"
+                        :key="`${r}-${c}`"
+                        :col="col"
+                        :row="row"
+                    />
+                    <TableActions
+                        v-if="actions"
+                        :actions="actions"
+                        :endpoint="endpoint"
+                    />
+                </tr>
+            </tbody>
+        </table>
+    </div>
+</template>
+
+<script>
+import TableRows from "./TableRows";
+import TableActions from "./TableActions";
+
+export default {
+    props: ["endpoint", "actions", "cols", "rows"],
+
+    mounted() {},
+
+    components: {
+        TableRows,
+        TableActions,
+    },
+};
+</script>
+
+<style scoped>
+.data-table {
+    @apply w-full shadow-lg rounded-xl font-bold;
+}
+.data-table td {
+    @apply h-16 px-5;
+}
+.data-table tbody td {
+    @apply border-t-2;
+}
+</style>
---
+++ resources/assets/js/components/DataTable/TableRows.vue
@@ -0,0 +1,19 @@
+<template>
+    <td>
+        <span
+            v-if="col.value == 'state'"
+            class="bg-gray-100 text-primary-500 rounded-full px-5 py-2"
+        >
+            {{ row[col.value].text }}
+        </span>
+        <template v-else>
+            {{ row[col.value] }}
+        </template>
+    </td>
+</template>
+
+<script>
+export default {
+    props: ["col", "row"],
+};
+</script>
